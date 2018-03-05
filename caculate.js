a2a15f85e0ec6209ff9c56242ca60d8e2f85716e
lostLittleCat <wanglingke7733@163.com>
2018-03-05 19:08:56 +0800

    regexp

--- caculate.js
+++ caculate.js
@@ -53,3 +53,55 @@ function order(words){
    }).join(' ');
 } 
 console.log(order("is2 Thi1s T4est 3a"))
+
+
+function likes (names) {
+  var templates = [
+    'no one likes this',
+    '{name} likes this',
+    '{name} and {name} like this',
+    '{name}, {name} and {name} like this',
+    '{name}, {name} and {n} others like this'
+  ];
+  var idx = Math.min(names.length, 4);
+  
+  return templates[idx].replace(/{name}|{n}/g, function (val) {
+    return val === '{name}' ? names.shift() : names.length;
+  });
+}
+
+
+//索引从1开始，计算一个正整数每个位上数字的索引次方与本身相同的
+function add(i){
+	let results = [];
+	for(let i = a;i<b;i++){
+		[].reduce.call([...String(i)],(pre,next,index)=>{console.log(next);return pre + Math.pow(next,index+1)},0);
+		result === i ? results.push(result):"";
+	}
+	return results;
+}
+
+var moveZeros = function (arr) {
+  let arrNoZero = [].filter.call(arr,(v,i)=>v!==0),
+      originLen = arr.length,
+      nowLen = arrNoZero.length;
+  return arrNoZero.concat(new Array(originLen - nowLen).fill(0))
+}
+
+
+
+function revrot(str, sz) {
+    if(sz<=0||str===""){
+      return ""
+    }else if(sz>str.length){
+      return ""
+    }
+    let reg = new RegExp("\\d{"+ sz +"}","g");
+    let chunks = str.match(reg),result = [];
+    for(let i = 0;i<chunks.length;i++){
+      let rest = [].reduce.call(chunks[i],(pre,next,index)=>pre+Math.pow(next,2),0)%2
+      let trans = rest === 1 ? chunks[i].slice(1)+chunks[i][0] : [].reverse.call([...chunks[i]]).join("")
+      result.push(trans);
+    }
+    return result.join("");
+}
