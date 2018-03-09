05569e5fcfefc6280c35d5958a35a678f871a0a6
lostLittleCat <wanglingke7733@163.com>
2018-03-09 14:01:07 +0800

    LCS

--- caculate.js
+++ caculate.js
@@ -206,4 +206,40 @@ function solution(roman){
     return now;
   },0) 
 }
-console.log(solution('MMVIII'))
+
+
+function LCS( xstr, ystr ) {
+  if( xstr == '' || ystr == '' ) return '';
+    
+  var xp    = xstr.charAt( 0 ), xrest = xstr.slice( 1 ),
+      yp    = ystr.charAt( 0 ), yrest = ystr.slice( 1 );
+      
+  if( xp == yp )
+    return xp + LCS( xrest, yrest );
+  
+  var lcs1 = LCS( xstr, yrest ),
+      lcs2 = LCS( xrest, ystr );
+      
+  return (lcs1.length > lcs2.length) ? lcs1 : lcs2;
+}
+//by司徒正美
+function LCS(str1, str2){
+    var m = str1.length 
+    var n = str2.length
+    var dp = [new Array(n+1).fill(0)] //第一行全是0
+    for(var i = 1; i <= m; i++){ //一共有m+1行
+        dp[i] = [0] //第一列全是0
+        for(var j = 1; j <= n; j++){//一共有n+1列
+            if(str1[i-1] === str2[j-1]){ 
+                //注意这里，str1的第一个字符是在第二列中，因此要减1，str2同理
+                dp[i][j] = dp[i-1][j-1] + 1 //对角＋1
+            } else {
+                 dp[i][j] = Math.max( dp[i-1][j], dp[i][j-1]) 
+            }
+        }
+    } 
+    return dp[m][n];
+}
+
+
+
