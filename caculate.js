5b8075d6bf7aa3b6b1484e9413c69321ab03e3e3
lostLittleCat <wanglingke7733@163.com>
2018-04-10 16:27:04 +0800

    quicksort

--- caculate.js
+++ caculate.js
@@ -456,3 +456,34 @@ function whichYear(num){
       dizhi = "子丑寅卯辰巳午未申酉戌亥";
   return "今年是"+tiangan[restTian]+dizhi[restDi]+"年";
 }
+//by 阮一峰
+var quickSort = function(arr) {
+
+　　if (arr.length <= 1) { return arr; }
+
+　　var pivotIndex = Math.floor(arr.length / 2);
+
+　　var pivot = arr.splice(pivotIndex, 1)[0];
+
+　　var left = [];
+
+　　var right = [];
+
+　　for (var i = 0; i < arr.length; i++){
+
+　　　　if (arr[i] < pivot) {
+
+　　　　　　left.push(arr[i]);
+
+　　　　} else {
+
+　　　　　　right.push(arr[i]);
+
+　　　　}
+
+　　}
+
+　　return quickSort(left).concat([pivot], quickSort(right));
+
+};
+
